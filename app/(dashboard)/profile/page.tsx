980aa34931b45ed76186987c983cc68904c3836d
Majazul Haque <142217528+ermajaz@users.noreply.github.com>
2025-11-19 20:20:18 +0530

    Refactor code structure for improved readability and maintainability

--- app/(dashboard)/profile/page.tsx
+++ app/(dashboard)/profile/page.tsx
@@ -1,9 +1,28 @@
-// src/app/(dashboard)/profile/page.tsx
+"use client";
+
+import ActivitySection from "@/features/profile/components/ActivitySection";
+import ProfileAddressSection from "@/features/profile/components/ProfileAddressSection";
+import ProfileHeader from "@/features/profile/components/ProfileHeader";
+import ProfileInfoCard from "@/features/profile/components/ProfileInfoCard";
+import ProfileStats from "@/features/profile/components/ProfileStats";
+import { motion } from "framer-motion";
+
+
 export default function ProfilePage() {
   return (
-    <div>
-      <h1 className="text-2xl font-semibold mb-4">My Profile</h1>
-      {/* profile component */}
-    </div>
+    <main className="min-h-screen text-white px-6 md:px-16 py-30">
+      <motion.div
+        initial={{ opacity: 0, y: 40 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.6, ease: "easeOut" }}
+        className="max-w-6xl mx-auto space-y-10"
+      >
+        <ProfileHeader/>
+        <ProfileStats />
+        <ProfileInfoCard />
+        <ProfileAddressSection />
+        <ActivitySection />
+      </motion.div>
+    </main>
   );
 }
